cb0e59264022e253d6dca87d5b60c32ee6ba9c00
Armando Herrera <anhydrous99@gmail.com>
2019-05-13 08:59:03 -0500

    Got OpenCV 4.1.0 working

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -7,80 +7,70 @@
 
 using namespace cv;
 
-extern "C" {
-
-IplImage *image_to_ipl(image im)
-{
-    int x,y,c;
-    IplImage *disp = cvCreateImage(cvSize(im.w,im.h), IPL_DEPTH_8U, im.c);
-    int step = disp->widthStep;
-    for(y = 0; y < im.h; ++y){
-        for(x = 0; x < im.w; ++x){
-            for(c= 0; c < im.c; ++c){
-                float val = im.data[c*im.h*im.w + y*im.w + x];
-                disp->imageData[y*step + x*im.c + c] = (unsigned char)(val*255);
-            }
-        }
-    }
-    return disp;
-}
-
-image ipl_to_image(IplImage* src)
-{
-    int h = src->height;
-    int w = src->width;
-    int c = src->nChannels;
-    image im = make_image(w, h, c);
-    unsigned char *data = (unsigned char *)src->imageData;
-    int step = src->widthStep;
-    int i, j, k;
-
-    for(i = 0; i < h; ++i){
-        for(k= 0; k < c; ++k){
-            for(j = 0; j < w; ++j){
-                im.data[k*w*h + i*w + j] = data[i*step + j*c + k]/255.;
-            }
-        }
-    }
-    return im;
-}
-
-Mat image_to_mat(image im)
+Mat image_to_mat(const image &im)
 {
     image copy = copy_image(im);
     constrain_image(copy);
     if(im.c == 3) rgbgr_image(copy);
 
-    IplImage *ipl = image_to_ipl(copy);
-    Mat m = cvarrToMat(ipl, true);
-    cvReleaseImage(&ipl);
+    int h = im.h;
+    int w = im.w;
+    int c = im.c;
+    Mat m(h, w, CV_8UC(c));
+    int x, y, k;
+    int step = m.step;
+
+    for (y = 0; y < h; ++y) {
+        for (x = 0; x < w; ++x) {
+            for (k = 0; k < c; ++k) {
+                float val = im.data[k*h*w + y*w + x];
+                m.data[y*step + x*c + k] = (unsigned char)(val*255);
+            }
+        }
+    }
+
     free_image(copy);
     return m;
 }
 
-image mat_to_image(Mat m)
-{
-    IplImage ipl = m;
-    image im = ipl_to_image(&ipl);
+image mat_to_image(const Mat &m) {
+    int h = m.size().height;
+    int w = m.size().width;
+    int c = m.channels();
+    image im = make_image(w, h, c);
+    unsigned char *data = m.data;
+    int step = m.step;
+    int i, j, k;
+
+    for (i = 0; i < h; ++i) {
+        for (k = 0; k < c; ++k) {
+            for (j = 0; j < w; ++j) {
+                im.data[k * w * h + i * w + j] = data[i * step + j * c + k] / 255.;
+            }
+        }
+    }
+
     rgbgr_image(im);
     return im;
 }
+
+extern "C" {
 
 void *open_video_stream(const char *f, int c, int w, int h, int fps)
 {
     VideoCapture *cap;
     if(f) cap = new VideoCapture(f);
     else cap = new VideoCapture(c);
-    if(!cap->isOpened()) return 0;
-    if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
-    if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
-    if(fps) cap->set(CV_CAP_PROP_FPS, w);
+    if(!cap->isOpened()) return nullptr;
+    if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
+    if(h) cap->set(CAP_PROP_FRAME_HEIGHT, w);
+    if(fps) cap->set(CAP_PROP_FPS, w);
     return (void *) cap;
 }
 
 image get_image_from_stream(void *p)
 {
-    VideoCapture *cap = (VideoCapture *)p;
+    auto *cap = (VideoCapture *)p;
     Mat m;
     *cap >> m;
     if(m.empty()) return make_empty_image(0,0,0);
@@ -123,7 +113,7 @@ void make_window(char *name, int w, int h, int fullscreen)
 {
     namedWindow(name, WINDOW_NORMAL); 
     if (fullscreen) {
-        setWindowProperty(name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+        setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
     } else {
         resizeWindow(name, w, h);
         if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
